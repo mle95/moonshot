b70f34188efe56d9dc75d8982fd4a7952dfdf656
mle95 <52006181+mle95@users.noreply.github.com>
2022-05-07 20:29:46 -0400

    Add files via upload
    add new class CustomerModel()

--- customer/models.py
+++ customer/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 
 class MenuItem(models.Model):
@@ -7,6 +9,7 @@ class MenuItem(models.Model):
     image = models.ImageField(upload_to='menu_images/')
     price = models.DecimalField(max_digits=5, decimal_places=2)
     category = models.ManyToManyField('Category', related_name='item')
+    ratings = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -32,7 +35,22 @@ class OrderModel(models.Model):
     state = models.CharField(max_length=15, blank=True)
     zip_code = models.IntegerField(blank=True, null=True)
     is_paid = models.BooleanField(default=False)
+    bidding = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
 
+
+class CustomerModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50, blank=True)
+    street = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    state = models.CharField(max_length=15, blank=True)
+    zip_code = models.IntegerField(blank=True, null=True)
+    balance = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f'Customer: {self.created_on.strftime("%b %d %I: %M %p")}'
