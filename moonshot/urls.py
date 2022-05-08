8197ca16e16be196f9227e61a37b98345c816b8d
mle95 <52006181+mle95@users.noreply.github.com>
2022-05-07 20:35:09 -0400

    Add files via upload
    add path('customer_info/') and revise 'about' view

--- moonshot/urls.py
+++ moonshot/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from customer.views import Index, About, Menu_Display, Order, OrderConfirmation, OrderPayConfirmation
+from customer.views import Index, About, Menu_Display, Customer_Info, Order, OrderConfirmation, OrderPayConfirmation
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,11 +25,10 @@ urlpatterns = [
     path('customer/', include('customer.urls')),
     path('restaurant/', include('restaurant.urls')),
     path('', Index.as_view(), name='index'),
-    path('about/', About.as_view(), name='about'),
+    path('about/<int:pk>', About.as_view(), name='about'),
     path('menu_display/', Menu_Display.as_view(), name='menu_display'),
+    path('customer_info/', Customer_Info.as_view(), name='customer_info'),
     path('order/', Order.as_view(), name='order'),
-    path('order-confirmation/<int:pk>', OrderConfirmation.as_view(),
-         name='order-confirmation'),
-    path('payment-confirmation/', OrderPayConfirmation.as_view(),
-         name='payment-confirmation'),
+    path('order-confirmation/<int:pk>', OrderConfirmation.as_view(), name='order-confirmation'),
+    path('payment-confirmation/', OrderPayConfirmation.as_view(),  name='payment-confirmation'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
