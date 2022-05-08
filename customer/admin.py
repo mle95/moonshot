0bb8c9d126984b6030ce96e3b7e548c21de69e97
mle95 <52006181+mle95@users.noreply.github.com>
2022-05-08 14:46:24 -0400

    Add files via upload
    added new model to admin/ page 
    admin.site.register(CustomerModel)

--- customer/admin.py
+++ customer/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import MenuItem, Category, OrderModel
+from .models import MenuItem, Category, OrderModel, CustomerModel
 
 admin.site.register(MenuItem)
 admin.site.register(Category)
 admin.site.register(OrderModel)
-
+admin.site.register(CustomerModel)
