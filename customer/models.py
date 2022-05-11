ac15ec4432f1b12f4db978dd2c263fd480fb01c0
mle95 <52006181+mle95@users.noreply.github.com>
2022-05-11 15:42:58 -0400

    Add files via upload
    add data fields 'orders_count' and 'total_spending'

--- customer/models.py
+++ customer/models.py
@@ -24,11 +24,11 @@ class Category(models.Model):
 
 class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
     items = models.ManyToManyField(
         'MenuItem', related_name='order', blank=True)
     name = models.CharField(max_length=50, blank=True)
-    email = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=50, null=False)
     phone = models.CharField(max_length=50, blank=True)
     street = models.CharField(max_length=50, blank=True)
     city = models.CharField(max_length=50, blank=True)
@@ -46,7 +46,7 @@ class OrderModel(models.Model):
 class CustomerModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=50, blank=True)
-    email = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=50, null=False)
     phone = models.CharField(max_length=50, blank=True)
     street = models.CharField(max_length=50, blank=True)
     city = models.CharField(max_length=50, blank=True)
@@ -55,6 +55,9 @@ class CustomerModel(models.Model):
     balance = models.DecimalField(max_digits=5, decimal_places=2)
     VIP_status = models.IntegerField(blank=True, null=True)
     warnings = models.IntegerField(blank=True, null=True)
+    orders_count = models.IntegerField(default=0, null=False)
+    total_spending = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    delivery_fee = models.BooleanField(default=True)
 
     def __str__(self):
         return f'Customer: {self.created_on.strftime("%b %d %I: %M %p")}'
