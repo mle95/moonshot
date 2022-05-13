64c110aaeb6d4a78ae5e97bef1a6663f68206a99
mle95 <52006181+mle95@users.noreply.github.com>
2022-05-13 14:29:42 -0400

    Add files via upload
    delivery drivers
    class DriverModel(models.Model)

--- customer/models.py
+++ customer/models.py
@@ -2,7 +2,6 @@ from django.db import models
 #from django.contrib.auth.models import AbstractUser
 
 
-
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -58,6 +57,23 @@ class CustomerModel(models.Model):
     orders_count = models.IntegerField(default=0, null=False)
     total_spending = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     delivery_fee = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Customer: {self.created_on.strftime("%b %d %I: %M %p")}'
+
+class DriverModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=50, null=False)
+    phone = models.CharField(max_length=50, blank=True)
+    street = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    state = models.CharField(max_length=15, blank=True)
+    zip_code = models.IntegerField(blank=True, null=True)
+    warnings = models.IntegerField(blank=True, null=True)
+    orders_count = models.IntegerField(default=0, null=False)
+    order_email = models.CharField(max_length=50, null=False)
+    bidding_price = models.IntegerField(blank=True, null=False)
 
     def __str__(self):
         return f'Customer: {self.created_on.strftime("%b %d %I: %M %p")}'
